77e154d2db88d561b9cd3dbbb969aa2af5f84155
hadley <h.wickham@gmail.com>
2010-09-05 11:45:21 -0500

    Add tests

---
+++ inst/tests/test-cast.r
@@ -0,0 +1,43 @@
+context("cast")
+
+s2 <- array(seq.int(3 * 4), c(3,4))
+s2m <- melt(s2)
+colnames(s2m) <- c("X1", "X2", "value")
+
+s3 <- array(seq.int(3 * 4 * 5), c(3,4,5))
+s3m <- melt(s3)
+colnames(s3m) <- c("X1", "X2", "X3", "value")
+
+test_that("reshaping matches t and aperm", {
+  # 2d 
+  expect_equivalent(s2, acast(s2m, X1 ~ X2))
+  expect_equivalent(t(s2), acast(s2m, X2 ~ X1))
+  expect_equivalent(as.vector(s2), as.vector(acast(s2m, X2 + X1 ~ .)))
+
+  # 3d 
+  expect_equivalent(s3, acast(s3m, X1 ~ X2 ~ X3))
+  expect_equivalent(as.vector(s3), as.vector(acast(s3m, X3 + X2 + X1 ~ .)))
+  expect_equivalent(aperm(s3, c(1,3,2)), acast(s3m, X1 ~ X3 ~ X2))
+  expect_equivalent(aperm(s3, c(2,1,3)), acast(s3m, X2 ~ X1 ~ X3))
+  expect_equivalent(aperm(s3, c(2,3,1)), acast(s3m, X2 ~ X3 ~ X1))
+  expect_equivalent(aperm(s3, c(3,1,2)), acast(s3m, X3 ~ X1 ~ X2))
+  expect_equivalent(aperm(s3, c(3,2,1)), acast(s3m, X3 ~ X2 ~ X1))
+})
+
+test_that("aggregation is matches apply", {
+
+  # 2d -> 1d
+  expect_equivalent(colMeans(s2), as.vector(acast(s2m, X2 ~ ., mean)))
+  expect_equivalent(rowMeans(s2), as.vector(acast(s2m, X1 ~ ., mean)))
+  
+  # 3d -> 1d 
+  expect_equivalent(apply(s3, 1, mean), as.vector(acast(s3m, X1 ~ ., mean)))
+  expect_equivalent(apply(s3, 1, mean), as.vector(acast(s3m, . ~ X1, mean)))
+  expect_equivalent(apply(s3, 2, mean), as.vector(acast(s3m, X2 ~ ., mean)))
+  expect_equivalent(apply(s3, 3, mean), as.vector(acast(s3m, X3 ~ ., mean)))
+  
+  # 3d -> 2d
+  expect_equivalent(apply(s3, c(1,2), mean), acast(s3m, X1 ~ X2, mean))
+  expect_equivalent(apply(s3, c(1,3), mean), acast(s3m, X1 ~ X3, mean))
+  expect_equivalent(apply(s3, c(2,3), mean), acast(s3m, X2 ~ X3, mean))
+})
---
+++ tests/test-all.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(reshape)
+
+test_package("reshape")
