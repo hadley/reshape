60cb9abe306e5d1c9429d1a23a31347768fea212
Michael Chirico <michaelchirico4@gmail.com>
2025-10-02 22:39:08 -0700

    really clear example of structural missingness --> fill

--- tests/testthat/test-cast.r
+++ tests/testthat/test-cast.r
@@ -222,4 +222,27 @@ test_that("mix of fun.aggregate= and fill=", {
     dcast(DF, group ~ variable, sum, na.rm = TRUE, fill = -1),
     data.frame(group = c("1", "2"), value = c(1, 0))
   )
+
+  # "structural" missingness, i.e., combinations that didn't exist
+  DF <- data.frame(
+    group = c("1", "2"),
+    variable = c("v1", "v2"),
+    value = c(1, NA)
+  )
+  expect_equal(
+    dcast(DF, group ~ variable, sum),
+    data.frame(group = c("1", "2"), v1 = c(1, 0), v2 = c(0, NA))
+  )
+  expect_equal(
+    dcast(DF, group ~ variable, sum, na.rm = TRUE),
+    data.frame(group = c("1", "2"), v1 = c(1, 0), v2 = c(0, 0))
+  )
+  expect_equal(
+    dcast(DF, group ~ variable, sum, fill = -1),
+    data.frame(group = c("1", "2"), v1 = c(1, -1), v2 = c(-1, NA))
+  )
+  expect_equal(
+    dcast(DF, group ~ variable, sum, na.rm = TRUE, fill = -1),
+    data.frame(group = c("1", "2"), v1 = c(1, -1), v2 = c(-1, 0))
+  )
 })
